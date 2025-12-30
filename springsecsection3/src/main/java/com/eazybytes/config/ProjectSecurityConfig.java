eb3050b3507b6088467f38ee451b9367fb934af3
Moumita Ray <moumitacloud08@gmail.com>
2025-12-30 23:06:42 +0530

    springsection3 password encoder

--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,10 +32,20 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
+		//UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+		//UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+		
 		UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-		UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+		UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$13$RwjUuTCRXjZXiPDnlHm5P.vDtvsHaqN6UfRxsJEe0GG1yhgkAVt4.").authorities("admin").build();
 		return new InMemoryUserDetailsManager(user, admin);
 
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		//return new BCryptPasswordEncoder(); //by default
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
 	}
 
 }
