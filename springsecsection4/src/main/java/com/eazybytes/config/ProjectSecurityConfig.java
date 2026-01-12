4580158f991c73f9c7cf5575d9b5e73025e9f81a
Moumita Ray <moumitacloud08@gmail.com>
2026-01-12 22:17:26 +0530

    adding jdbc

---
+++ springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.eazybytes.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+						.authenticated().requestMatchers("/notices", "/contact", "/error").permitAll());
+
+		http.formLogin(withDefaults()); // If Form style login is required
+		http.httpBasic(withDefaults());
+		return http.build();
+
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(DataSource datasource) {
+		return new JdbcUserDetailsManager(datasource);
+
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+	}
+	
+	@Bean
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+		return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
+
+}
