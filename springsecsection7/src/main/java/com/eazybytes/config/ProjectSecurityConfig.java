144cbb43721b0c6e89e77cf7e40580e35fba0732
Moumita Ray <moumitacloud08@gmail.com>
2026-01-27 16:14:43 +0530

    section7

---
+++ springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.eazybytes.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import com.eazybytes.exceptionhandling.CustomAccessDeniedhandler;
+import com.eazybytes.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+		.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(3).maxSessionsPreventsLogin(true))
+		.requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) //only HTTP
+		.csrf(csrfConfig -> csrfConfig.disable())
+		       .authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+						.authenticated().requestMatchers("/notices", "/contact", "/error","/register","/invalidSession").permitAll());
+
+		http.formLogin(withDefaults()); // If Form style login is required
+		http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));  // only for login
+		//http.exceptionHandling(ehc -> ehc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint())); //Global config,  for all the flow
+		//http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedhandler()).accessDeniedPage("/denied"));
+		http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedhandler()));
+		return http.build();
+
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+	}
+	
+	@Bean
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+		return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
+
+}
